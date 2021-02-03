61dcab890549ec3548368258fc8c302ba728341b
unknown <nahidukil547@gmail.com>
2021-02-03 20:23:37 +0600

    update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:false  ,
+    margin:10,
+    nav:false,
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1 
+        },
+        900:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
