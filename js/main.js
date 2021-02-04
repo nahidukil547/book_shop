e16db0a45c7f5252cd62402f04061b21a4d5a3f2
unknown <nahidukil547@gmail.com>
2021-02-04 10:51:06 +0600

    60% finish

--- js/main.js
+++ js/main.js
@@ -10,9 +10,13 @@ $('.owl-carousel').owlCarousel({
         600:{
             items:1 
         },
-        900:{
+        770:{
+            items:2
+        },
+        1000:{
             items:3
         },
+        
         1200:{
             items:4
         }
